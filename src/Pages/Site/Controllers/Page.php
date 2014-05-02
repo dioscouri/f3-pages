d066f8b2df4d845b8940a0883d52c4ffe978f929
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-05-02 08:47:07 -0400

    made pages respect publication rules, fixes dioscouri/amritasingh-f3#46

--- src/Pages/Site/Controllers/Page.php
+++ src/Pages/Site/Controllers/Page.php
@@ -12,12 +12,13 @@ class Page extends \Dsc\Controller
     public function read()
     {
     	// TODO get the slug param.  lookup the pages page.  Check ACL against page.
-    	
     	$f3 = \Base::instance();
     	$slug = $this->inputfilter->clean( $f3->get('PARAMS.slug'), 'cmd' );
     	$model = $this->getModel()->populateState()
-            ->setState('filter.slug', $slug);
-    	
+            ->setState('filter.slug', $slug)
+    	   ->setState( 'filter.published_today', true )
+    	   ->setState( 'filter.publication_status', 'published' );
+    	    	
     	try {
     		$item = $model->getItem();
     	} catch ( \Exception $e ) {
@@ -26,7 +27,14 @@ class Page extends \Dsc\Controller
     		$f3->reroute( '/' );
     		return;
     	}
-    	
+
+    	if( empty( $item ) ){
+    	    // TODO Change to a normal 404 error
+    	    \Dsc\System::instance()->addMessage( "Invalid Page", 'error');
+    	    $f3->reroute( '/' );
+    	    return;
+    	}
+    	 
     	\Base::instance()->set('pagetitle', $item->title);
     	\Base::instance()->set('subtitle', '');
     	
