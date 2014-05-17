e07f669c174c80134c33112ad3b8fe276d079c91
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-05-17 10:06:18 -0400

    updated menu item adder

--- src/Pages/Admin/Listener.php
+++ src/Pages/Admin/Listener.php
@@ -1,49 +1,56 @@
-<?php 
+<?php
 namespace Pages\Admin;
 
-class Listener extends \Prefab 
+class Listener extends \Prefab
 {
-    public function onSystemRebuildMenu( $event )
+
+    public function onSystemRebuildMenu($event)
     {
-    	if ($model = $event->getArgument('model'))
-    	{
-    		$root = $event->getArgument( 'root' );
-    		$pages = clone $model;
-    	
-    		$pages->insert(
-    				array(
-    						'type'	=> 'admin.nav',
-    						'priority' => 40,
-    						'title'	=> 'Pages',
-    						'icon'	=> 'fa fa-file-text',
-        					'is_root' => false,
-    						'tree'	=> $root,
-							'base' => '/admin/pages',
-    				)
-    		);
-    		
+        if ($model = $event->getArgument('model'))
+        {
+            $root = $event->getArgument('root');
+            $pages = clone $model;
+            
+            $pages->insert(array(
+                'type' => 'admin.nav',
+                'priority' => 40,
+                'title' => 'Pages',
+                'icon' => 'fa fa-file-text',
+                'is_root' => false,
+                'tree' => $root,
+                'base' => '/admin/pages'
+            ));
+            
             $children = array(
-                    array( 'title'=>'Pages', 'route'=>'/admin/pages/pages', 'icon'=>'fa fa-list' ),
-                    array( 'title'=>'Categories', 'route'=>'/admin/pages/categories', 'icon'=>'fa fa-folder' ),
-                    //array( 'title'=>'Settings', 'route'=>'/admin/pages/settings', 'icon'=>'fa fa-cogs' ),
+                array(
+                    'title' => 'Pages',
+                    'route' => '/admin/pages/pages',
+                    'icon' => 'fa fa-list'
+                ),
+                array(
+                    'title' => 'Categories',
+                    'route' => '/admin/pages/categories',
+                    'icon' => 'fa fa-folder'
+                )
             );
-           	$pages->addChildrenItems( $children, $root );
+            
+            $pages->addChildren($children, $root);
             
             \Dsc\System::instance()->addMessage('Pages added its admin menu items.');
         }
     }
-    
-    public function onAdminNavigationGetQuickAddItems( $event )
+
+    public function onAdminNavigationGetQuickAddItems($event)
     {
         $items = $event->getArgument('items');
         $tree = $event->getArgument('tree');
-
-        $item = new \stdClass;
+        
+        $item = new \stdClass();
         $item->title = 'Pages';
         $item->form = \Pages\Admin\Controllers\MenuItemQuickAdd::instance()->page($event);
         $items[] = $item;
         
-        $item = new \stdClass;
+        $item = new \stdClass();
         $item->title = 'Pages Category';
         $item->form = \Pages\Admin\Controllers\MenuItemQuickAdd::instance()->category($event);
         $items[] = $item;
