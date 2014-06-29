ec8845108aef19f22e9d5781bfbd3d33dc00c907
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-06-29 12:51:25 -0400

    added publication status to search result item

--- src/Pages/Models/Pages.php
+++ src/Pages/Models/Pages.php
@@ -133,14 +133,15 @@ class Pages extends \Dsc\Mongo\Collections\Content
     public function toAdminSearchItem()
     {
         $image = (!empty($this->{'featured_image.slug'})) ? './asset/thumb/' . $this->{'featured_image.slug'} : null;
-    
+        $published_status = '<span class="label ' . $this->publishableStatusLabel() . '">' . $this->{'publication.status'} . '</span>';
+        
         $item = new \Search\Models\Item(array(
             'url' => './admin/pages/page/edit/' . $this->id,
             'title' => $this->title,
             'subtitle' => 'By: ' . $this->{'metadata.creator.name'},
             'image' => $image,
             'summary' => $this->getAbstract(),
-            'datetime' => 'Published: ' . date('Y-m-d', $this->{'publication.start.time'} )
+            'datetime' => $published_status . ' ' . date('Y-m-d', $this->{'publication.start.time'} ),
         ));
     
         return $item;
