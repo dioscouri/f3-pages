f8f308a1ef04f6f17d1162b7bceee14881a3fd19
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-06-30 09:31:42 -0400

    curated priority of search scopes

--- bootstrap.php
+++ bootstrap.php
@@ -2,28 +2,31 @@
 class PagesBootstrap extends \Dsc\Bootstrap
 {
     protected $dir = __DIR__;
+
     protected $namespace = 'Pages';
-    
+
     protected function preSite()
     {
         if (class_exists('\Search\Factory'))
         {
-            \Search\Factory::registerSource( new \Search\Models\Source( array(
-                'id'=>'pages', 'title'=>'Pages', 'class'=>'\Pages\Models\Pages'
-            ) ) );            
-        }        
-
+            \Search\Factory::registerSource(new \Search\Models\Source(array(
+                'id' => 'pages',
+                'title' => 'Pages',
+                'class' => '\Pages\Models\Pages'
+            )));
+        }
     }
-    
+
     protected function preAdmin()
     {
         if (class_exists('\Search\Factory'))
         {
-            \Search\Factory::registerSource( new \Search\Models\Source( array(
-                'id'=>'pages', 'title'=>'Pages', 'class'=>'\Pages\Models\Pages'
-            ) ) );
-            
+            \Search\Factory::registerSource(new \Search\Models\Source(array(
+                'id' => 'pages',
+                'title' => 'Pages',
+                'class' => '\Pages\Models\Pages',
+            )));
         }
-    }    
+    }
 }
 $app = new PagesBootstrap();
