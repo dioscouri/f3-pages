7e87e9c05ec5b2b6bee753393d02d88e2900b7de
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2015-04-02 11:43:53 -0400

    added some display config options to default pages view

--- src/Pages/Admin/Views/pages/fields_display.php
+++ src/Pages/Admin/Views/pages/fields_display.php
@@ -1,48 +1,70 @@
 <div class="row">
     <div class="col-md-2">
-        
-        <h3>Display</h3>
+    
+        <h3>Options</h3>
+        <p class="help-block">Define how the item should appear.</p>
                 
     </div>
     <!-- /.col-md-2 -->
                 
     <div class="col-md-10">
-    
-        <div class="portlet">
 
-            <div class="portlet-header">
-
-                <h3>View File</h3>
-                <p class="help-block">Which file should be used to display this page?</p>
-
-            </div>
-            <!-- /.portlet-header -->
-
-            <div class="portlet-content">
-            
-                <div class="input-group">
-                    <select name="display[view]" class="form-control">
-                        <option value="" <?php if (!$flash->old('display.view')) { echo "selected"; } ?>>-- Default --</option>
-                        <?php $variants = \Dsc\System::instance()->get('theme')->variants( 'Pages/Site/Views::pages/view.php' ); ?>
-                        <?php foreach ($variants as $group=>$views) { ?>
-                            <optgroup label="<?php echo $group; ?>">
-                                <?php foreach ($views as $view) { ?>
-                                <option value="<?php echo $view; ?>" <?php if ($flash->old('display.view') == $view) { echo "selected"; } ?>><?php echo $view; ?></option>
-                                <?php } ?>
-                            </optgroup>
-                        <?php } ?>
-                    </select> 
-                </div>
-                <!-- /.form-group -->
-
-            </div>
-            <!-- /.portlet-content -->
-
+        <div class="form-group">
+            <label>Hide the Title?</label>
+            <select name="display[title_disabled]" class="form-control">
+                <option value="0" <?php if (!$flash->old('display.title_disabled')) { echo "selected"; } ?>>No</option>
+                <option value="1" <?php if ($flash->old('display.title_disabled')) { echo "selected"; } ?>>Yes, hide it</option>
+            </select>
         </div>
-        <!-- /.portlet -->
+        <!-- /.form-group -->
         
+        <div class="form-group">
+            <label>Where to put the Featured Image?</label>
+            <select name="display[image_position]" class="form-control">
+                <option value="banner" <?php if ($flash->old('display.image_position') == 'banner') { echo "selected"; } ?>>A banner, above the page</option>
+                <option value="top" <?php if ($flash->old('display.image_position') == 'top') { echo "selected"; } ?>>Within the page, at the top</option>
+                <option value="below-title" <?php if ($flash->old('display.image_position') == 'below-title') { echo "selected"; } ?>>Within the page, below the title</option>
+            </select>
+        </div>
+        <!-- /.form-group -->
+    
     </div>
     <!-- /.col-md-10 -->
-    
 </div>
 <!-- /.row -->
+
+<hr/>
+
+<div class="row">
+    <div class="col-md-2">
+    
+        <h3>Template</h3>
+        <p class="help-block">Which template should be used to display this page?</p>
+                
+    </div>
+    <!-- /.col-md-2 -->
+                
+    <div class="col-md-10">
+
+        <div class="form-group">
+            <label>Select a Template</label>
+            <select name="display[view]" class="form-control">
+                <option value="" <?php if (!$flash->old('display.view')) { echo "selected"; } ?>>-- Default --</option>
+                <?php $variants = \Dsc\System::instance()->get('theme')->variants( 'Pages/Site/Views::pages/view.php' ); ?>
+                <?php foreach ($variants as $group=>$views) { ?>
+                    <optgroup label="<?php echo $group; ?>">
+                        <?php foreach ($views as $view) { ?>
+                        <option value="<?php echo $view; ?>" <?php if ($flash->old('display.view') == $view) { echo "selected"; } ?>><?php echo $view; ?></option>
+                        <?php } ?>
+                    </optgroup>
+                <?php } ?>
+            </select>
+        </div>
+        <!-- /.form-group -->
+    
+    </div>
+    <!-- /.col-md-10 -->
+</div>
+<!-- /.row -->
+
+<hr/>
--- src/Pages/Site/Views/pages/view.php
+++ src/Pages/Site/Views/pages/view.php
@@ -1,9 +1,27 @@
+<?php if (!empty($item->{'featured_image.slug'}) && (!$item->{'display.image_position'} || $item->{'display.image_position'} == 'banner')) { ?>
+    <div class="banner">
+        <img class="img-responsive" src="./asset/<?php echo $item->{'featured_image.slug'}; ?>" alt="">
+    </div>
+<?php } ?>
+
 <article id="page-<?php echo $item->id; ?>" class="page-<?php echo $item->id; ?>">
 
     <div class="entry-header">
+        <?php if (!empty($item->{'featured_image.slug'}) && ($item->{'display.image_position'} == 'top')) { ?>
+            <p>
+                <img class="img-responsive" src="./asset/<?php echo $item->{'featured_image.slug'}; ?>" alt="">
+            </p>
+        <?php } ?>    
+        <?php if (empty($item->{'display.title_disabled'})) { ?>
         <h2 class="entry-title">
             <?php echo $item->{'title'}; ?>
         </h2>
+        <?php } ?>
+        <?php if (!empty($item->{'featured_image.slug'}) && ($item->{'display.image_position'} == 'below-title')) { ?>
+            <p>
+                <img class="img-responsive" src="./asset/<?php echo $item->{'featured_image.slug'}; ?>" alt="">
+            </p>
+        <?php } ?>
     </div>
 
     <div class="entry-description">
