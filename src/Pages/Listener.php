5623f096f5c4156a9b11c5955134a48d81332920
Chris French <chris@ammonitenetworks.com>
2015-08-29 15:59:36 -0500

    allowing tags

--- src/Pages/Listener.php
+++ src/Pages/Listener.php
@@ -4,4 +4,24 @@ namespace Pages;
 class Listener extends \Prefab 
 {
 
+	
+	function siteMapRegisterRoutes($event) {
+		$routes = $event->getArgument('routes');
+		 
+		 
+		$slugs = (new \Pages\Models\Pages)->collection()->distinct('slug', ['publication.status' => ['$ne' => 'unpublished']]);
+		 
+		//products
+		$pageRoutes = array_map( function($val) { return ['loc' => '/pages/'.$val,
+				'pri' => '1.0',
+				'change' => 'weekly',
+				'mod' => null ];
+		}, $slugs);
+		//
+		
+		$routes['pages'] = $pageRoutes;
+		 
+		$event->setArgument('routes', $routes);
+		 
+	}
 }
